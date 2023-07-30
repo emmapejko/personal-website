1fd47cb31c3a23ded1c02a39e1a9dc4af1b184c2
emmapejko <emma.pejko@gmail.com>
2023-07-30 12:27:07 -0700

    add home page

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  ChakraProvider
+} from '@chakra-ui/react'
 
 import App from './components/App.jsx';
 
 const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
